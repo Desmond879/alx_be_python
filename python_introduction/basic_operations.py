09ec993343b1e294053ae99a6786e39048794485
Bunu Desmond Efiyemi <peresdesmond9792@gmil.com>
2024-09-15 16:50:23 +0100

    Add basic_operations.py for basic arithmetic exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+substraction = number1 - number2
+multiplecation = number1 * number2
+
+print (f"addition of{number1} and {number2} is {addition}")
+print (f"substraction of{number1} and {number2} is {substraction}")
+print (f"multiplecation of{number1} and {number2} is {multiplecation}")
+
